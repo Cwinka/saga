87ac6342caefa2593b837fca86e8668274ce391e
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-09-05 20:52:37 +0500

    test: Added test for compensation functions

--- tests/test_saga_worker.py
+++ tests/test_saga_worker.py
@@ -31,3 +31,35 @@ def test_worker_job_run(worker):
 def test_worker_job_err(worker):
     with pytest.raises(SpecialErr):
         worker.job(run_in_worker_with_raise).run()
+
+
+def test_worker_job_with_compensation(worker):
+    compensate_check = 0
+    x = 42
+
+    def foo(_x: str) -> None:
+        nonlocal compensate_check
+        compensate_check = int(_x)
+
+    with pytest.raises(SpecialErr):
+        with worker.compensate():
+            worker.job(run_in_worker, x).with_compensation(foo).run()
+            worker.job(run_in_worker_with_raise).run()
+
+    assert compensate_check == x, 'Компенсационная функция не была запущена.'
+
+
+def test_worker_job_with_compensation_no_run(worker):
+    compensate_check = 0
+    x = 42
+
+    def foo(_x: str) -> None:
+        nonlocal compensate_check
+        compensate_check = int(_x)
+
+    with pytest.raises(SpecialErr):
+        worker.job(run_in_worker, x).with_compensation(foo).run()
+        worker.job(run_in_worker_with_raise).run()
+
+    assert compensate_check == 0, 'Компенсационная функция была запущена без контекстного ' \
+                                  'менеджера.'
