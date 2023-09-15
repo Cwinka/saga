6e107e41b6f1afca830aedab80f459f582d668db
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-09-15 18:13:11 +0500

    test: Добавлен тест журнала MemoryJournal

---
+++ tests/test_journal.py
@@ -0,0 +1,32 @@
+from saga.journal import MemoryJournal
+from saga.models import JobRecord
+
+
+def test_get():
+    record = JobRecord('1')
+    dct = {'1': record}
+    journal = MemoryJournal(dct)
+    get_record = journal.get_record('1')
+    assert get_record is not None
+    assert get_record.idempotent_operation_id == record.idempotent_operation_id
+
+
+def test_create():
+    key = '1'
+    dct = {}
+    journal = MemoryJournal(dct)
+    created_record = journal.create_record(key)
+    assert isinstance(created_record, JobRecord)
+    assert created_record.idempotent_operation_id == key
+    assert dct[key] is created_record
+
+
+def test_update():
+    key = '1'
+    record = JobRecord(key)
+    dct = {}
+    journal = MemoryJournal(dct)
+
+    record.payload = b'42'
+    journal.update_record(record)
+    assert dct[key] is record
