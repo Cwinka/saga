2f4c4a67c74adafcd1b4d91b4134dd2735010dcb
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-09-21 11:36:32 +0500

    fix(tests): Исправлен тест для проверки SagaRunner

--- tests/test_saga_runner.py
+++ tests/test_saga_runner.py
@@ -1,9 +1,13 @@
+import pytest
+
 from saga.models import Ok
-from saga.saga import SagaJob, SagaRunner
+from saga.saga import SagaJob, SagaRunner, idempotent_saga
 from saga.worker import SagaWorker
 
 
 def test_saga_runner_new(saga_journal, wk_journal):
+
+    @idempotent_saga('foo')
     def foo(_worker: SagaWorker, _) -> int:
         return 1
 
@@ -12,3 +16,9 @@ def test_saga_runner_new(saga_journal, wk_journal):
     job = fk.new('1', foo, Ok())
     assert isinstance(job, SagaJob)
 
+
+def test_saga_runner_wrong_saga(saga_journal, wk_journal):
+    fk = SagaRunner(saga_journal, wk_journal)
+
+    with pytest.raises(AssertionError):
+        job = fk.new('1', lambda x, y: 1, Ok())
