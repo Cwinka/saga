32338711376357172fde93cb641031b61c47e689
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-09-21 12:59:06 +0500

    feat: Добавлены верхнеуровневые импорты

--- saga/__init__.py
+++ saga/__init__.py
@@ -1,14 +1,33 @@
 from .compensator import SagaCompensator
-from .journal import WorkerJournal, MemoryJournal
-from .saga import SagaJob, SagaRunner
-from .worker import SagaWorker, WorkerJob
+from .events import CommunicationFactory, RedisCommunicationFactory, SagaEvents, \
+    SocketCommunicationFactory
+from .journal import MemoryJournal, MemorySagaJournal, SagaJournal, WorkerJournal
+from .models import Event, EventSpec, JobRecord, JobStatus, Ok, SagaRecord
+from .saga import SagaRunner, idempotent_saga
+from .worker import SagaWorker
 
 __all__ = [
-    'SagaRunner',
-    'SagaJob',
-    'SagaWorker',
-    'WorkerJob',
     'SagaCompensator',
+
+    'SagaEvents',
+    'CommunicationFactory',
+    'RedisCommunicationFactory',
+    'SocketCommunicationFactory',
+
+    'SagaJournal',
+    'MemorySagaJournal',
     'WorkerJournal',
     'MemoryJournal',
+
+    'Event',
+    'EventSpec',
+    'JobRecord',
+    'JobStatus',
+    'Ok',
+    'SagaRecord',
+
+    'SagaRunner',
+    'idempotent_saga',
+
+    'SagaWorker',
 ]
