cedbf79e710307c1c696608f64cd1e4775e61b7e
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-10-05 20:09:26 +0500

    refactor: Разделены классы WorkerJob и SagaWorker

--- saga/worker.py
+++ saga/worker.py
@@ -1,5 +1,5 @@
 import functools
-from typing import Any, Callable, Generic, Optional, ParamSpec, Tuple, TypeVar
+from typing import Any, Callable, Optional, ParamSpec, Tuple, TypeVar
 from uuid import UUID
 
 from saga.compensator import SagaCompensator
@@ -7,82 +7,11 @@ from saga.events import EventSender
 from saga.journal import WorkerJournal
 from saga.memo import Memoized
 from saga.models import Event, In, JobSpec, NotAnEvent, Ok, Out
+from saga.worker_job import WorkerJob
 
 P = ParamSpec('P')
 T = TypeVar('T')
-C = TypeVar('C')
-CompensationCallback = Callable[[JobSpec[C]], None]
 SAGA_KEY_SEPARATOR = '&'
-
-
-class WorkerJob(Generic[T, C]):
-    """
-    `WorkerJob` унифицированный объект для запуска функций:
-
-        def any_function() -> int:
-            ...
-
-        job = WorkerJob(JobSpec(any_function, *args, **kwargs))
-        job.run()
-
-    Для того, чтобы откатить результат выполнения функции `any_function` необходимо добавить
-    компенсирующую функцию, которая первым аргументом принимает возвращаемое значение
-    `any_function`:
-
-        def rollback_any_function(result_of_any_function: int) -> None:
-            ...
-
-        job.with_compensation(rollback_any_function)
-        job.run()
-
-        ...
-
-        job.compensate()
-    """
-
-    def __init__(self, spec: JobSpec[T],
-                 comp_set_callback: CompensationCallback[C] = lambda *_: None) -> None:
-        """
-        :param spec: Спецификация функции.
-        :param comp_set_callback: Обратный вызов компенсации, вызывается перед выполнением spec
-                                  и если установлена функция компенсации.
-        """
-        self._spec = spec
-        self._compensation_callback = comp_set_callback
-        self._compensation_spec: Optional[JobSpec[C]] = None
-        self._run: bool = False
-        self._crun: bool = False
-
-    def run(self) -> T:
-        """
-        Выполнить spec функцию. Метод можно выполнить только один раз.
-
-        :return: Результат spec функции.
-        """
-        assert not self._run, 'Повторный вызов функции не позволен.'
-        if self._compensation_spec:
-            self._compensation_callback(self._compensation_spec)
-        self._run = True
-        return self._spec.call()
-
-    def with_compensation(self, spec: JobSpec[C]) -> 'WorkerJob[T, C]':
-        """
-        Установить функцию компенсации.
-
-        :param spec: Спецификации компенсационной функции.
-        :return: Тот же объект `WorkerJob`.
-        """
-        self._compensation_spec = spec
-        return self
-
-    def compensate(self) -> None:
-        """
-        Запустить компенсацию, если она существует. Метод можно выполнить только один раз.
-        """
-        assert self._run, 'Функция не была вызвана. Нечего компенсировать.'
-        assert not self._crun, 'Повторный вызов компенсационной функции не позволен.'
-        if self._compensation_spec is not None:
-            self._compensation_spec.call()
 
 
 class SagaWorker:
