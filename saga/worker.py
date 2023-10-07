f7110be2bc836cd74576298a1251f522436549be
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-10-07 23:29:59 +0500

    feat: Добавлено логирование в модуль worker.py

--- saga/worker.py
+++ saga/worker.py
@@ -5,6 +5,7 @@ from uuid import UUID
 from saga.compensator import SagaCompensator
 from saga.events import EventSender
 from saga.journal import WorkerJournal
+from saga.logger import logger
 from saga.memo import Memoized
 from saga.models import Event, In, JobSpec, NotAnEvent, Ok, Out
 from saga.worker_job import WorkerJob
@@ -46,6 +47,7 @@ class SagaWorker:
         :param compensation_event_timeout: Время ожидания события компенсации.
         """
         self._uuid = uuid
+        self._saga_name = saga_name
         self._idempotent_key = join_key(uuid, saga_name)
         self._memo = Memoized(self._idempotent_key, journal)
         self._sender = sender
@@ -54,6 +56,7 @@ class SagaWorker:
         self._compensation_max_retries = compensation_max_retries
         self._compensation_interval = compensation_interval
         self._compensation_event_timeout = compensation_event_timeout
+        self._w_prefix = f'[W: {uuid} S: {saga_name}]'
 
     @property
     def idempotent_key(self) -> str:
@@ -73,12 +76,14 @@ class SagaWorker:
         """
         Запустить все компенсационные функции.
         """
+        logger.info(f'{self._w_prefix} Запуск заданий компенсаций.')
         self._compensate.run()
 
     def forget_done(self) -> None:
         """
         Удалить все добавленные записи в журнал, позволяя запуск с тем же идемпотентным ключом.
         """
+        logger.info(f'{self._w_prefix} Удаление записей выполнение заданий из журнала.')
         self._memo.forget_done()
 
     def job(self, spec: JobSpec[T, P], retries: int = 1, retry_interval: float = 2.0)\
@@ -93,7 +98,9 @@ class SagaWorker:
                                функции в случае исключения.
         """
         spec.f = self._memo.memoize(spec.f, retries=retries, retry_interval=retry_interval)
-        return WorkerJob[T, None, P](spec, comp_set_callback=self._place_compensation)
+        logger.debug(f'{self._w_prefix} Создание простого задания с функцией "{spec.name}".')
+        return WorkerJob[T, None, P](spec, comp_set_callback=self._place_compensation,
+                                     uuid=self._uuid, saga_name=self._saga_name)
 
     def event_job(self, spec: JobSpec[Event[In, Out], P], retries: int = 1,
                   retry_interval: float = 2.0, timeout: float = 10.0) \
@@ -112,8 +119,10 @@ class SagaWorker:
         spec.f = self._memo.memoize(self._auto_send(spec.f,  # type: ignore[arg-type]
                                                     timeout=timeout),
                                     retries=retries, retry_interval=retry_interval)
+        logger.debug(f'{self._w_prefix} Создание событийного задания с функцией "{spec.name}".')
         return WorkerJob[Out, Event[Any, Any], P](spec,  # type: ignore[arg-type]
-                                                  comp_set_callback=self._place_event_compensation)
+                                                  comp_set_callback=self._place_event_compensation,
+                                                  uuid=self._uuid, saga_name=self._saga_name)
 
     def _place_event_compensation(self, spec: JobSpec[Event[Any, Ok], ...]) -> None:
         spec.f = self._memo.memoize(self._auto_send(spec.f,  # type: ignore[arg-type]
@@ -121,11 +130,13 @@ class SagaWorker:
                                                     cancel_previous_uuid=True),
                                     retries=self._compensation_max_retries,
                                     retry_interval=self._compensation_interval)
+        logger.debug(f'{self._w_prefix} Добавление событийного задания компенсации "{spec.name}".')
         self._compensate.add_compensate(spec)
 
     def _place_compensation(self, spec: JobSpec[None, ...]) -> None:
         spec.f = self._memo.memoize(spec.f, retries=self._compensation_max_retries,
                                     retry_interval=self._compensation_interval)
+        logger.debug(f'{self._w_prefix} Добавление простого задания компенсации "{spec.name}".')
         self._compensate.add_compensate(spec)
 
     def _auto_send(self, f: Callable[P, Event[Any, Out]], timeout: float,
